15fcb5747b20195ad1bbacb6479390bd3313586d
Bryan Carlie Lukito Setiawan <blukitosetiawan@gmail.com>
2026-05-01 11:25:08 +0700

    refactor: remove dynamic component-based vite loading from app layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,7 +41,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Tangerine:wght@400;700&display=swap" rel="stylesheet">
 
         @viteReactRefresh
-        @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         <x-inertia::head>
             <title>{{ config('app.name', 'Laravel') }}</title>
         </x-inertia::head>
